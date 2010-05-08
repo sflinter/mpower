966f656c7f39e942eec980cd081d5d03d519cf0d
Steve Flinter <stephen@flinter.com>
2010-05-08 22:36:17 +0100

    Added database & deployment scripts

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,40 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :repo_user, "sflinter"
+set :user, 'flintero'
+set :domain, 'mpower.flinter.org'
+set :host, "aether.site5.com"
+set :application, "mpower"
+# set :repository,  "#{repo_user}@flinter.org:/home/#{repo_user}/git/#{application}.git"
+# set :repository, "file:///home/flintero/git/mpower.git"
+# set :local_repository, "file:///home/flintero/git/mpower.git"
+set :repository,  "git://github.com/#{repo_user}/#{application}.git"
+set :deploy_to, "/home/#{user}/#{application}"
+#default_run_options[:pty] = true
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+# misc options
+set :scm, 'git'
+set :deploy_via, :remote_cache
+set :branch, 'master'
+set :scm_verbose, true
+set :use_sudo, false
+set :group_writable, false
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :keep_releases, 10
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+role :app, host
+role :web, host
+role :db,  host, :primary => true
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  desc "Restart the web server"
+  task :restart, :roles => :app do
+    run "cd /home/#{user}/public_html ; rm #{domain} ; ln -s #{current_path}/public ./#{domain}"
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared do
+    run "ln -nsf #{shared_path}/config/config.yml #{release_path}/config/config.yml"
+  end
+end
+
+after 'deploy:update_code', 'deploy:symlink_shared'
